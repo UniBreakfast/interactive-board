39d5d783198f30e3334c8bf786e794fbee90c828
oksana-shpak <oksana.shpak@convertdigital.com.au>
2024-11-08 19:43:00 +1100

    first commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const http = require('http');
+const fs = require('fs');
+const WebSocket = require('ws');
+
+// Create HTTP server
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    fs.readFile('index.html', (err, content) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Error loading index.html');
+        return;
+      }
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content);
+    });
+  }
+});
+
+// Create WebSocket server
+const wss = new WebSocket.Server({ server });
+
+// Store connected clients
+const clients = new Set();
+
+// Handle WebSocket connections
+wss.on('connection', (ws) => {
+  clients.add(ws);
+  
+  // Handle incoming messages
+  ws.on('message', (message) => {
+    // Broadcast the message to all other clients
+    const data = message.toString();
+    clients.forEach(client => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
+  
+  // Handle client disconnection
+  ws.on('close', () => {
+    clients.delete(ws);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
